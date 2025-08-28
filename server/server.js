3aad8feaa55f096abac53255f5a66a394a1a6554
ayahalij <ayahali2003@gmail.com>
2025-08-28 09:25:13 +0300

    update

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/database');
 require('dotenv').config();
 
@@ -12,12 +13,24 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
 
-// Routes
+// Serve static files from the React build directory
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Serve images and audio from the public directory
+app.use('/images', express.static(path.join(__dirname, '../client/public/images')));
+app.use('/audio', express.static(path.join(__dirname, '../client/public/audio')));
+
+// API Routes
 app.use('/api/cards', require('./routes/cards'));
 app.use('/api/comments', require('./routes/comments'));
 
+// Serve React app for all non-API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
 });
