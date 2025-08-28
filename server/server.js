d8a557a455d22d4dda41d379cbb841ae85fe9daf
ayahalij <ayahali2003@gmail.com>
2025-08-28 10:20:51 +0300

    work in my device

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const connectDB = require('./config/database');
 require('dotenv').config();
 
@@ -13,24 +12,12 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
-// Serve static files from the React build directory
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-// Serve images and audio from the public directory
-app.use('/images', express.static(path.join(__dirname, '../client/public/images')));
-app.use('/audio', express.static(path.join(__dirname, '../client/public/audio')));
-
-// API Routes
+// Routes
 app.use('/api/cards', require('./routes/cards'));
 app.use('/api/comments', require('./routes/comments'));
 
-// Serve React app for all non-API routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
 });
