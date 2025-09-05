f51c91f3608f412950b5bfe95012b42f88c34e1d
ayahalij <ayahali2003@gmail.com>
2025-09-05 23:48:10 +0300

    ...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Home from './pages/Home';
+// import Home from './pages/Home';
 import './styles/App.css';
 
 function App() {
+  console.log('App component is rendering');
+  
   return (
     <Router>
       <div className="App">
-        <Home />
+        <h1>Digital Scrapbook</h1>
+        <p>App is working! The issue is likely in the Home component.</p>
       </div>
     </Router>
   );
